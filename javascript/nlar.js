ef4b8f7d346f17bd3ac8ddb5d4927599d5232860
Van Thuan <83982994+vthuan@users.noreply.github.com>
2022-03-02 12:54:24 +0700

    Update nlar.js

--- javascript/nlar.js
+++ javascript/nlar.js
@@ -18,7 +18,7 @@ app.controller('myController', function($scope) {
 	$scope.modal4 = "Tên Tài khoản";
 	$scope.modal5 = "UID Genshin Impact";
 	$scope.linkfb = "https://www.facebook.com/vthuan.1412/";
-	$scope.linkzl = "m.me/vthuan.1412";
+	$scope.linkzl = "https://m.me/vthuan.1412";
 	$scope.linksc = "https://t.me/tkswhyred";
 	$scope.linkgm = "takeshi@gmail.com";
 	$scope.phonenumber = "849730274";
